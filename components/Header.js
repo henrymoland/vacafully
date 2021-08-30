568c843519fe165ad1ddda427497873f557ce213
Henry Moland <henrymoland@gmail.com>
2021-08-30 07:02:29 -0700

    Styled search input

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import logo from "../assets/vacafully_logo.svg";
+import { SearchIcon } from "@heroicons/react/solid";
 
 function Header() {
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      {/* Logo */}
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image
           src={logo}
@@ -13,7 +15,15 @@ function Header() {
         />
       </div>
 
-      <div></div>
+      {/* Search Input */}
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input
+          className="flex-grow pl-5 bg-transparent outline-none"
+          type="text"
+          placeholder="Where are you going?"
+        />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-blue-500 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+      </div>
 
       <div></div>
     </header>
