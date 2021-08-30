c2160f3f919ceae0db2f647a58948f948f88ff98
Henry Moland <henrymoland@gmail.com>
2021-08-29 21:13:41 -0700

    Installed Hero Icons

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import logo from "../assets/logo.svg";
+import logo from "../assets/vacafully_logo.svg";
 
 function Header() {
   return (
-    <header>
-      <div>
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image
           src={logo}
           layout="fill"
