8b426528aedaf2614e1bfbeee8ec73bd967664df
Henry Moland <henrymoland@gmail.com>
2021-09-01 10:37:20 -0700

    Created testimonial card component for guest testimonials

---
+++ components/TestimonialCard.js
@@ -0,0 +1,11 @@
+function TestimonialCard({
+  image,
+  testimonial,
+  guestImg,
+  guestName,
+  guestLocation,
+}) {
+  return <div></div>;
+}
+
+export default TestimonialCard;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import TestimonialCard from "../components/TestimonialCard";
 
-export default function Home() {
+export default function Home({ testimonialData }) {
   return (
     <div className="">
       <Head>
@@ -12,6 +13,39 @@ export default function Home() {
 
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-3xl font-semibold pb-5">
+            What guests are saying about homes in United States
+          </h2>
+
+          {testimonialData?.map(
+            ({ image, testimonial, guest, name, location }) => (
+              <TestimonialCard
+                key={image}
+                img={image}
+                testimonial={testimonial}
+                guestImg={guest}
+                guestName={name}
+                guestLocation={location}
+              />
+            )
+          )}
+        </section>
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const testimonialData = await fetch("https://jsonkeeper.com/b/46TW").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      testimonialData,
+    },
+  };
 }
