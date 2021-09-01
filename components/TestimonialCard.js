72abd2cafc793714a2766c739d008dfb71330cbd
Henry Moland <henrymoland@gmail.com>
2021-09-01 14:09:37 -0700

    Styled guest testimonial section

--- components/TestimonialCard.js
+++ components/TestimonialCard.js
@@ -1,11 +1,48 @@
+import Image from "next/image";
+import { StarIcon } from "@heroicons/react/solid";
+
 function TestimonialCard({
-  image,
+  img,
   testimonial,
   guestImg,
   guestName,
   guestLocation,
 }) {
-  return <div></div>;
+  return (
+    <div className="mt-5 md:w-[370px]">
+      {/* Top */}
+      <div className="relative h-80 w-full md:h-64">
+        <Image
+          src={img}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-lg"
+          alt="Property image"
+        />
+      </div>
+
+      {/* Middle */}
+      <div className="flex mt-3">
+        {[...Array(5)].map((star) => {
+          return <StarIcon className="text-blue-400 h-6" />;
+        })}
+      </div>
+
+      <p className="mt-2 leading-relaxed">{testimonial}</p>
+
+      {/* Bottom */}
+      <div className="flex mt-4">
+        <div className="relative h-14 w-14 mr-4">
+          <Image src={guestImg} layout="fill" className="rounded-full" />
+        </div>
+
+        <div>
+          <p className="font-bold">{guestName}</p>
+          <p className="text-gray-500">{guestLocation}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TestimonialCard;
--- pages/index.js
+++ pages/index.js
@@ -16,22 +16,29 @@ export default function Home({ testimonialData }) {
 
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
-          <h2 className="text-3xl font-semibold pb-5">
+          <h2 className="text-3xl font-semibold pb-2">
             What guests are saying about homes in United States
           </h2>
+          <p className="text-gray-500 text-lg">
+            Over <span className="font-bold">65,500,000 guest reviews</span>{" "}
+            with an average of{" "}
+            <span className="font-bold">4.8 out of 5 stars</span>
+          </p>
 
-          {testimonialData?.map(
-            ({ image, testimonial, guest, name, location }) => (
-              <TestimonialCard
-                key={image}
-                img={image}
-                testimonial={testimonial}
-                guestImg={guest}
-                guestName={name}
-                guestLocation={location}
-              />
-            )
-          )}
+          <div className="md:flex justify-between">
+            {testimonialData?.map(
+              ({ image, testimonial, guest, name, location }) => (
+                <TestimonialCard
+                  key={image}
+                  img={image}
+                  testimonial={testimonial}
+                  guestImg={guest}
+                  guestName={name}
+                  guestLocation={location}
+                />
+              )
+            )}
+          </div>
         </section>
       </main>
     </div>
